2fc7b3338d826da7c676d1b7891858e7daf9c9e5
Pallavi-varekar <pallavivarekar4@gmail.com>
2022-10-13 22:31:05 +0530

    created uc7 duplicate entry

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,5 +1,20 @@
 package com.bridgelabz;
 
-public class AddressBook {
+import java.util.ArrayList;
 
+public class AddressBook {
+    ArrayList<Contact> addressbook = new ArrayList<Contact>();
+
+    public ArrayList<Contact> addContact(Contact contact) {
+        this.addressbook.add(contact);
+        return this.addressbook;
+    }
+
+    public ArrayList<Contact> getAddressbook() {
+        return addressbook;
+    }
+
+    public void setAddressbook(ArrayList<Contact> addressbook) {
+        this.addressbook = addressbook;
+    }
 }
